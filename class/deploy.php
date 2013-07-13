bca99ef97aefb05e986024a0f804775999f72628
Jabberwocked <de_jabberwock@hotmail.com>
2013-07-13 11:43:35 +0200

    updated folders

--- class/deploy.php
+++ class/deploy.php
@@ -40,7 +40,7 @@ define('BRANCH', 'master');
  *
  * @var string Full path including the trailing slash
  */
-define('TARGET_DIR', '/tmp/simple-php-git-deploy/');
+define('TARGET_DIR', '/volume1/web/Miljoenenidee/');
 
 /**
  * Weather to delete the files that are not in the repository but are on the
