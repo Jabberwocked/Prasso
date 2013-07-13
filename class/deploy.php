7042928a69d1cd0b336adc4a0bf76e2cbd285ed7
Jabberwocked <de_jabberwock@hotmail.com>
2013-07-13 18:20:50 +0200

    Change-Id: Id27c421dc6bf135da6b91fe6ec28f210b1adfbdb

--- class/deploy.php
+++ class/deploy.php
@@ -12,10 +12,7 @@ $commands = array(
 	'echo $PWD',
 	'whoami',
 	'git pull',
-	'git status',
-	'git submodule sync',
-	'git submodule update',
-	'git submodule status');
+	'git status');
 
 // Run the commands for output
 $output = '';
