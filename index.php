f54c07bac7fc4a094a5d32708f5df40b707fe2ac
Jabberwocked <de_jabberwock@hotmail.com>
2013-07-22 20:18:53 +0200

    please log in

--- index.php
+++ index.php
@@ -36,6 +36,36 @@ if (isset($_SESSION['HTTP_USER_AGENT']))
 
 		<?php 
 	}
+	else 
+	{
+		?>
+				
+				<!DOCTYPE html>
+				<html>
+				<head>
+				<title>Welcome to Prasso. Please log in.</title>
+				<link rel="stylesheet" type="text/css" href="style.css" />
+				</head>
+				
+				<body>
+				
+					<header id="head">
+						<p>Prasso: User Login</p>
+						<p>
+							<a href="register.php"><span id="register">Register</span></a>
+						</p>
+					</header>
+				
+					<div id="main-wrapper">
+						<p> please log in</p>
+					</div>
+				</body>
+				</html>
+		
+				<?php 
+	}
+		
+		
 }
 else
 {
