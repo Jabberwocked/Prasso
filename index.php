8c18da020b2e3319cc9bac6753af140f48a59efc
Jabberwocked <de_jabberwock@hotmail.com>
2013-07-22 19:59:35 +0200

    Simple test with sessions.

--- index.php
+++ index.php
@@ -2,6 +2,66 @@
 include_once ("config.php");
 ?>
 
+
+<?php
+
+session_start();
+
+if (isset($_SESSION['HTTP_USER_AGENT']))
+{
+    if ($_SESSION['HTTP_USER_AGENT'] != md5($_SERVER['HTTP_USER_AGENT']))
+    { ?>
+    
+		<!DOCTYPE html>
+		<html>
+		<head>
+		<title>Welcome to Prasso. Please log in.</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		</head>
+		
+		<body>
+		
+			<header id="head">
+				<p>Prasso: User Login</p>
+				<p>
+					<a href="register.php"><span id="register">Register</span></a>
+				</p>
+			</header>
+		
+			<div id="main-wrapper">
+				<div id="login-wrapper">
+					<form method="post" action="">
+						<ul>
+							<li><label for="usn">Username : </label> <input type="text"
+								maxlength="30" required autofocus name="username" /></li>
+		
+							<li><label for="passwd">Password : </label> <input type="password"
+								maxlength="30" required name="password" /></li>
+							<li class="buttons"><input type="submit" name="login"
+								value="Log me in" /> <input type="button" name="register"
+								value="Register" onclick="location.href='register.php'" /></li>
+		
+						</ul>
+					</form>
+		
+				</div>
+			</div>
+		
+		</body>
+		</html>
+		
+		<?php
+        exit;
+    }
+}
+else
+{
+    $_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
+}
+
+?>
+
+
 <?php if( !(isset( $_POST['login'] ) ) ) { ?>
 
 <!DOCTYPE html>
