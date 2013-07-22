43349d1958cafa450fa8424ca300beacf5b4a5b0
Jabberwocked <de_jabberwock@hotmail.com>
2013-07-22 20:09:40 +0200

    Added some HTML To display on login

--- index.php
+++ index.php
@@ -10,13 +10,65 @@ session_start();
 if (isset($_SESSION['HTTP_USER_AGENT']))
 {
 	if ($_SESSION['HTTP_USER_AGENT'] != md5($_SERVER['HTTP_USER_AGENT']))
-	{
+	{ ?>
+		
+		<!DOCTYPE html>
+		<html>
+		<head>
+		<title>Welcome to Prasso. Please log in.</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		</head>
+		
+		<body>
+		
+			<header id="head">
+				<p>Prasso: User Login</p>
+				<p>
+					<a href="register.php"><span id="register">Register</span></a>
+				</p>
+			</header>
+		
+			<div id="main-wrapper">
+				<div id="login-wrapper">
+				</div>
+			</div>
+		</body>
+		</html>
+
+		<?php 
 		exit;
 	}
 }
 else
 {
 	$_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
+	?>
+			
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<title>Welcome to Prasso.</title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	
+	<body>
+	
+		<header id="head">
+			<p>Prasso</p>
+			<p>
+				<a href="register.php"><span id="register">Register</span></a>
+			</p>
+		</header>
+	
+		<div id="main-wrapper">
+			<div id="login-wrapper">
+			<p> welcome </p>
+			</div>
+		</div>
+	</body>
+	</html>
+
+	<?php 
 }
 
 ?>
