6fd62f8ca5ee99b0bc0eb0c55f05fc81428a6bc0
Teun <teuncortooms@gmail.com>
2013-08-04 13:04:40 +0100

    Update temp layout and site structure

--- temp_makeatest.php
+++ temp_makeatest.php
@@ -9,13 +9,16 @@
 <br><br><br><br>
 <div id="buttons">
 <a href="temp_makeatest_new.php"><button type="button">New</button></a><br>
+</div>
 <br><br><br>
+
+<div style="width:500px; margin-left:auto; margin-right:auto">
 <h1>History</h1><br>
 <br>
-4-8-2013	Present continuous	<a href="">Take</a>	<a href="">Edit</a>	<a href="">Results</a><br>
-2-8-2013	Present simple <a href="">Take</a>	<a href="">Edit</a>	<a href="">Results</a><br>
-
+4-8-2013	Present continuous	<a href="">Take</a>	<a href="">Edit</a>	<a href="">Results</a>	<a href="">Invite</a><br>
+2-8-2013	Present simple <a href="">Take</a>	<a href="">Edit</a>	<a href="">Results</a>	<a href="">Invite</a><br>
 </div>
+
 </body>
 </html>
 
--- temp_trackprogress.php
+++ temp_trackprogress.php
@@ -8,11 +8,16 @@
 <body>
 <br><br><br><br>
 <div id="buttons">
-<a href=""><button type="button" style="width:400px">My results by test</button></a><br>
+<a href=""><button type="button" style="width:450px">My results by test</button></a><br>
 <br><br><br>
-<a href=""><button type="button" style="width:400px">Other results by test</button></a><br>
-<a href=""><button type="button" style="width:400px">Other results by user</button></a><br>
+<a href=""><button type="button" style="width:450px">Other results by test</button></a><br>
+<a href=""><button type="button" style="width:450px">Other results by user</button></a><br>
+<br><br><br>
+<a href=""><button type="button" style="width:450px">Request to track another user</button></a><br>
+<a href=""><button type="button" style="width:450px">Invite a user to track me</button></a><br>
+
 </div>
 </body>
 </html>
 
+ 
