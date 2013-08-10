1b03b626e09ff4b86d9cb904e30e789d4f97aac2
Teun <teuncortooms@gmail.com>
2013-08-10 12:00:36 +0100

    progress menu update

--- teun/resources/templates/menu/menu_trackprogress.php
+++ teun/resources/templates/menu/menu_trackprogress.php
@@ -1,20 +1,15 @@
 	<center>
 		<ul class="sonarmenu">
-			<li><a href="">My results by test</a></li>
+			<li><a href="">My results</a></li>
+			<li><a href="">All results</a></li>
+			<br>
+			<li><a href="">Filter by user/test</a></li>
 			<br>
 			<br>
 			<br>
 			<br>
-			<li><a href="">Other results by test</a></li>
-			<br>
-			<li><a href="">Other results by user</a></li>
-			<br>
-			<br>
-			<br>
-			<br>
-			<li><a href="">Request to track another user</a></li>
-			<br>
-			<li><a href="">Invite a user to track me</a></li>
+			<li><a href="">Track a user</a></li>
+			<li><a href="">Invite tracker</a></li>
 			<br>
 		</ul>
 	</center>
