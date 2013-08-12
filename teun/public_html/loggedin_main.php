b02f76a2c51f489fb324b25d6f694b7d840d8f09
Teun <teuncortooms@gmail.com>
2013-08-12 22:15:32 +0100

    Revert "test"
    This reverts commit 255e4f7ec94c775970b240e148f1ee0acdf9cf48.

--- teun/public_html/loggedin_main.php
+++ teun/public_html/loggedin_main.php
@@ -3,4 +3,4 @@ include_once ("../resources/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
 include_once (MENU_PATH . "/menu_main_loggedin.php");
 include_once (TEMPLATES_PATH . "/footer.php");
-?> 
+?>
