a8f5a57c6ab4ec23de533d74e8c0af65d192c2c7
Teun <teuncortooms@gmail.com>
2013-08-14 23:48:04 +0100

    Layout update

--- teun/resources/templates/menu/menu_main_loggedin.php
+++ teun/resources/templates/menu/menu_main_loggedin.php
@@ -4,9 +4,11 @@
 		<ul class="sonarmenu">
 			<li><a href="loggedin_takeatest.php">Take a test</a></li>
 			<br>
-			<li><a href="loggedin_makeatest.php">Make a test</a></li>
-			<br>
 			<li><a href="loggedin_results.php">Results</a></li>
+			<br>
+			<br>
+			<br>
+			<li><a href="loggedin_makeatest.php">Make a test</a></li>
 			<br>
 			<li><a href="loggedin_other.php">Other</a></li>
 			<br>
