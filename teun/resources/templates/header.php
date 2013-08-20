6e410d6da5dd4637eced1fe7f8b66ace69c371ff
Teun <teuncortooms@gmail.com>
2013-08-20 12:44:00 +0100

    Updated header to indicate log status (placeholder)
    Just a placeholder name for now. Could say LOG IN. Requires php.

--- teun/resources/templates/header.php
+++ teun/resources/templates/header.php
@@ -6,8 +6,13 @@
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 <body>
-<div class='header'><a href="loggedin_main.php"><h1 id='title'>PRASSO</h1></a></div>
+	<div class='header'>
+		<a href="loggedin_main.php"><h1 id='title'>PRASSO</h1></a>
+		<a href="" style="position:absolute; top:55px; right:0px; font-size:15px">Guust Flater</a>
+	</div>
 	<br>
 	<br>
 	<br>
 	<br>
+</body>
+</html>
