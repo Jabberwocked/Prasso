adb7c301ec6c4bffb47bdf8fb3e7011d9a94144f
Teun <teuncortooms@gmail.com>
2013-08-23 15:06:53 +0200

    test (final)

--- teun/public_html/loggedin_makeatest_new.php
+++ teun/public_html/loggedin_makeatest_new.php
@@ -3,4 +3,4 @@ include_once ("../resources/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
 include_once (MENU_PATH . "/menu_makeatest_new.php");
 include_once (TEMPLATES_PATH . "/footer.php");
-?> 
+?>
