45715a18c5e3c48e57d7eff8227fe9641809ca64
Teun <teuncortooms@gmail.com>
2013-08-28 12:08:24 +0200

    backlog updated

--- templates/menu/menu_takeatest.php
+++ templates/menu/menu_takeatest.php
@@ -1,7 +1,7 @@
 
 	<center>
 		<nav>
-			<a href="">Random by topic</a><br>
+			<a href="loggedin_takeatest_random.php">Random by topic</a><br>
 			<a href="">My tests <!-- and shared tests --> </a><br>
 			<a href="">Administered tests</a>
 		</nav>
