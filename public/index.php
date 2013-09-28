7741ae56eba34d331676045d010c1570def50b05
teuncortooms <teuncortooms@gmail.com>
2013-09-28 13:23:10 +0200

    paths geupdatet

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-include_once ("config/config.php");
+include_once ("../config/config.php");
 
 // Start the session
 session_start();
@@ -31,7 +31,7 @@ else
 	$_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
 	
 	// This is a new session. Check if logged in.
-	header('Location: ./loggedin_main.php');
+	header('Location: ./main.php');
     exit();
 }
 ?>
