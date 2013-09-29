9aa23d2087b48ddffcce71b88fe26807c157270b
teuncortooms <teuncortooms@gmail.com>
2013-09-29 18:21:23 +0200

    centered

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -3,62 +3,62 @@ include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
 ?>
 
-
-
-<form action="takeatest_random.php" method="get">
-	Filter by type<br> 
-	<input type="radio" style="display:inline; width:20px;" name="type" value="all">All<br>
-	<input type="radio" style="display:inline; width:20px;" name="type" value="shortanswer">Short Answer<br>
-	<input type="radio" style="display:inline; width:20px;" name="type" value="multichoice">Multiple Choice<br>
+<div style="text-align:center">
+	
+	<form action="takeatest_random.php" method="get">
+		Filter by type<br> 
+		<input type="radio" style="display:inline; width:20px;" name="type" value="all">All<br>
+		<input type="radio" style="display:inline; width:20px;" name="type" value="shortanswer">Short Answer<br>
+		<input type="radio" style="display:inline; width:20px;" name="type" value="multichoice">Multiple Choice<br>
+		<br>
+		How many questions would you like to answer?<br> 
+		<input type="text" name="number" value="0"> 
+		<br> 
+		<input type="submit" value="Generate Test">
+	</form>
 	<br>
-	How many questions would you like to answer?<br> 
-	<input type="text" name="number" value="0"> 
-	<br> 
-	<input type="submit" value="Generate Test">
-</form>
-<br>
-<br>
-
-<form>
-
-
-<?php
-$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-
-$sql = "SELECT * FROM Questions";
-$results = $db->query($sql);
-
-$n = 0;
-$number = $_GET["number"];
-$type = $_GET["type"];
-
-foreach ($results as $row)
-{
-	if ($n < $number)
+	<br>
+	
+	<form>
+	
+	
+	<?php
+	$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+	
+	$sql = "SELECT * FROM Questions";
+	$results = $db->query($sql);
+	
+	$n = 0;
+	$number = $_GET["number"];
+	$type = $_GET["type"];
+	
+	foreach ($results as $row)
 	{
-		if ($type == "all" or $row['Type'] == $type)
+		if ($n < $number)
 		{
-			$n ++;
-			echo "<p style='font-weight:bold;'>Question " . $n . "</p><br>";
-			echo "<p>" . $row['Question'] . "</p><br><br>";
-			echo "<input type='text' name='" . $row['QuestionId'] . "' ><br>"; 			
+			if ($type == "all" or $row['Type'] == $type)
+			{
+				$n ++;
+				echo "<p style='font-weight:bold;'>Question " . $n . "</p><br>";
+				echo "<p>" . $row['Question'] . "</p><br><br>";
+				echo "<input type='text' name='" . $row['QuestionId'] . "' ><br>"; 			
+			}
+		}
+		else
+		{
+			break;
 		}
 	}
-	else
+	if($n < $number)
 	{
-		break;
+		echo "<p style='color:red'>There are no more questions of that type.</p><br><br>";
 	}
-}
-if($n < $number)
-{
-	echo "<p style='color:red'>There are no more questions of that type.</p><br><br>";
-}
-
-
-?>
-<input type="submit" value="Submit Answers">
-</form>
-
+	
+	
+	?>
+	<input type="submit" value="Submit Answers">
+	</form>
+</div>
 
 <?php
 include_once (TEMPLATES_PATH . "/footer.php");
