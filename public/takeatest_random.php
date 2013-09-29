462f577cce03914cf04d3be50fb141471d9578cc
teuncortooms <teuncortooms@gmail.com>
2013-09-29 16:53:47 +0200

    query test

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -6,30 +6,32 @@ include_once (TEMPLATES_PATH . "/header.php");
 
 
 <form action="takeatest_random.php" method="get">
-How many questions would you like to answer?<br>
-<input type="text" name="Numberofquestions" value="0">
-<br>
-<input type="submit" value="Submit">
+	Filter by type<br> <input type="radio" name="type" value="shortanswer">
+	<input type="radio" name="type" value="multichoice"> How many questions
+	would you like to answer?<br> <input type="text" name="number"
+		value="0"> <br> <input type="submit" value="Submit">
 </form>
 <br>
 <br>
 
-<?php 
+<?php
 $db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
 
 $sql = "SELECT * FROM Questions";
 $results = $db->query($sql);
 
-
-$number = $_GET["Numberofquestions"];
+$number = $_GET["number"];
 $n = 0;
 
 foreach ($results as $row)
 {
 	if ($n < $number)
 	{
-		echo $row['Question'] . '<br>';
-		$n ++;
+		if ($row['Type'] = $_GET["type"])
+		{
+			echo $row['Question'] . '<br>';
+			$n ++;
+		}
 	}
 	else
 	{
