ab806f51e81dd3a7683a922894bc4cd2d783d4b5
teuncortooms <teuncortooms@gmail.com>
2013-09-29 14:31:48 +0200

    query update

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -6,22 +6,6 @@ include_once (TEMPLATES_PATH . "/header.php");
 <?php
 
 
-/* $con = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-$sql = "SELECT * FROM Question WHERE QuestionId = 1";
-
-$stmt = $con->prepare($sql);
-
-echo $stmt;
-echo 'hi';
-
-$con = null;
-
-echo "hello hello"
- */
-
-echo 'hello1';
 
 $db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
 
@@ -33,7 +17,7 @@ foreach($results as $row)
 	echo $row['Question'].'<br>';
 }
 
-echo 'hello2';
+
 
 ?>
 
