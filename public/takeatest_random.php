86adb3e12985ebf2c70eb6f09d16d69f8f2d6cc8
teuncortooms <teuncortooms@gmail.com>
2013-09-30 21:32:14 +0200

    asdf

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -14,7 +14,18 @@ include_once (TEMPLATES_PATH . "/header.php");
 		<input type="checkbox" style="display:inline; width:20px;" name="type[]" value="multichoice" <? if(in_array("multichoice", $_GET['type'])){ echo 'checked';}?>>Multiple Choice<br>
 		<br>
 		How many questions would you like to answer?<br> 
-		<input type="number" name="number" value="<?php if($_GET['number'] > 0){ echo $_GET['number'];} else {echo 10} ?>"> 
+		<input type="number" name="number" value="
+			<?php 
+				if($_GET['number'] > 0)
+				{ 
+					echo $_GET['number'];
+				} 
+				else 
+				{
+					echo 10;
+				}
+			?>
+		"> 
 		<br> 
 		<input type="submit" value="Generate Test">
 	</form>
