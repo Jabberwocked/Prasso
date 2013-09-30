5dd68ea09baadde8db59f5f790b9f4f072ce361e
teuncortooms <teuncortooms@gmail.com>
2013-09-30 21:23:53 +0200

    fdsa

--- public/js/checkall.js
+++ public/js/checkall.js
@@ -1,27 +1,12 @@
 $(document).ready(function(){
 
 	
-	
-//	$( '.selectall' ).click( function () {
-//	    $( this ).closest( 'form' ).find( ':checkbox' ).attr( 'checked' , this.checked );
-//	});
-
-
-//	$('.selectall').change(function() {
-//	    var checkboxes = $(this).closest('form').find(':checkbox');
-//	    if($(this).is(':checked')) {
-//	        checkboxes.attr('checked', 'checked');
-//	    } else {
-//	        checkboxes.removeAttr('checked');
-//	    }
-//	});
-
-
 	$(function() {
 		   $('.selectall').change(function() {
 		      $(this).siblings('input[type=checkbox]').prop('checked', this.checked);
-		      // $(this).closest('section').find('input[type=checkbox]').prop('checked', this.checked);
 		   });
 		});
 
+	
+	
 });
--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -9,64 +9,61 @@ include_once (TEMPLATES_PATH . "/header.php");
 
 	<form action="takeatest_random.php" method="get">
 		Filter by type<br> 
-		<input type="checkbox" style="display:inline; width:20px;" class="selectall">All<br>
-		<input type="checkbox" style="display:inline; width:20px;" name="type[]" value="shortanswer" class="chk_boxes1" <? if(in_array("shortanswer", $_GET['type'])){ echo 'checked';}?>>Short Answer<br>
-		<input type="checkbox" style="display:inline; width:20px;" name="type[]" value="multichoice" class="chk_boxes1" <? if(in_array("multichoice", $_GET['type'])){ echo 'checked';}?>>Multiple Choice<br>
+		<input type="checkbox" style="display:inline; width:20px;" name="selectall" value="yes" class="selectall" <? if($_GET['selectall'] == 'yes'){ echo 'checked';}?>>All<br>
+		<input type="checkbox" style="display:inline; width:20px;" name="type[]" value="shortanswer" <? if(in_array("shortanswer", $_GET['type'])){ echo 'checked';}?>>Short Answer<br>
+		<input type="checkbox" style="display:inline; width:20px;" name="type[]" value="multichoice" <? if(in_array("multichoice", $_GET['type'])){ echo 'checked';}?>>Multiple Choice<br>
 		<br>
 		How many questions would you like to answer?<br> 
 		<input type="text" name="number" value="<?php echo $_GET['number']; ?>"> 
 		<br> 
 		<input type="submit" value="Generate Test">
 	</form>
-	
-	
-
-	
 	<br>
 	<br>
+	
 	
 	<form>
-	
-	
-	<?php
 		
-	$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+		<?php
+			
+		$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+	
+		$type = "'".implode("','", $_GET["type"])."'";
+		$number = $_GET["number"];
+		
+		$sql = "SELECT * FROM Questions WHERE Type IN (".$type.") ORDER BY RAND() LIMIT $number";
+		
+		
+		$results = $db->query($sql);
+		$n = 0;
+		
+		foreach ($results as $row)
+		{
+			$n ++;
+			echo "<p style='font-weight:bold;'>Question " . $n . "</p><br>";
+			echo "<p>" . $row['Question'] . "</p><br><br>";
+			echo "<input type='text' name='" . $row['QuestionId'] . "' ><br>";
+		}
+		
+		if($n < $number)
+		{
+			echo "<p style='color:red'>There are no more questions of that type.</p><br><br>";
+		}
+		
+		if($number > 0)
+		{
+			echo "<input type='submit' value='Submit Answers'>";
+		}
+	
+	
+		?>
+	
+	</form>
 
-	$type = "'".implode("','", $_GET["type"])."'";
-	$number = $_GET["number"];
-	
-	$sql = "SELECT * FROM Questions WHERE Type IN (".$type.") ORDER BY RAND() LIMIT $number";
-	
-	
-	$results = $db->query($sql);
-	$n = 0;
-	
-	foreach ($results as $row)
-	{
-		$n ++;
-		echo "<p style='font-weight:bold;'>Question " . $n . "</p><br>";
-		echo "<p>" . $row['Question'] . "</p><br><br>";
-		echo "<input type='text' name='" . $row['QuestionId'] . "' ><br>";
-	}
-	
-	if($n < $number)
-	{
-		echo "<p style='color:red'>There are no more questions of that type.</p><br><br>";
-	}
-	
-	if($number > 0)
-	{
-		echo "<input type='submit' value='Submit Answers'>";
-	}
-
-
-	?>
-</form>
-
-
-<br><br><br>
+	<br><br><br>
 	
 </div>
+
 
 <?php
 include_once (TEMPLATES_PATH . "/footer.php");
