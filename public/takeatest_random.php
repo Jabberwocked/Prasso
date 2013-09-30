37b8df26ba0fda132e90b6b10090f46054d987c7
teuncortooms <teuncortooms@gmail.com>
2013-09-30 13:55:40 +0200

    testing random

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -36,7 +36,7 @@ include_once (TEMPLATES_PATH . "/header.php");
 //	$sql = "SELECT * FROM Questions"; 
 // 	$sql = "SELECT * FROM Questions WHERE QuestionId >= RAND() * (SELECT MAX(QuestionId) FROM Questions)";
 // 	$sql = "SELECT * FROM Questions WHERE Type = '".$type."' LIMIT '".$number."'";
-	$sql = "SELECT * FROM Questions WHERE Type = '".$type."'";
+	$sql = "SELECT * FROM Questions WHERE Type = * ";
 	$results = $db->query($sql);
 	
 	
