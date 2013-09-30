2e43fd44512c02fb637c6b4193cd367f92e8a045
teuncortooms <teuncortooms@gmail.com>
2013-09-30 21:18:36 +0200

    adsfa

--- public/js/checkall.js
+++ public/js/checkall.js
@@ -7,16 +7,21 @@ $(document).ready(function(){
 //	});
 
 
-	$('.selectall').change(function() {
-	    var checkboxes = $(this).closest('form').find(':checkbox');
-	    if($(this).is(':checked')) {
-	        checkboxes.attr('checked', 'checked');
-	    } else {
-	        checkboxes.removeAttr('checked');
-	    }
-	});
+//	$('.selectall').change(function() {
+//	    var checkboxes = $(this).closest('form').find(':checkbox');
+//	    if($(this).is(':checked')) {
+//	        checkboxes.attr('checked', 'checked');
+//	    } else {
+//	        checkboxes.removeAttr('checked');
+//	    }
+//	});
 
 
-
+	$(function() {
+		   $('.selectall').change(function() {
+		      $(this).siblings('input[type=checkbox]').prop('checked', this.checked);
+		      // $(this).closest('section').find('input[type=checkbox]').prop('checked', this.checked);
+		   });
+		});
 
 });
