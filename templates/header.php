bd1f72ec7a9a562ed446edfada62beeb4ef7267f
teuncortooms <teuncortooms@gmail.com>
2013-09-30 21:08:44 +0200

    jquery.js

--- templates/header.php
+++ templates/header.php
@@ -4,16 +4,8 @@
 <meta charset="ISO-8859-1">
 <title>Prasso</title>
 <link rel="stylesheet" type="text/css" href="css/style.css" />
-
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js">
-</script>
-<script type="text/javascript">
-$(document).ready(function(){
-	$( '.selectall' ).click( function () {
-	    $( this ).closest( 'form' ).find( ':checkbox' ).attr( 'checked' , this.checked );
-	});
-});
-</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script src="js/jQuery.js"></script>
 
 
 </head>
