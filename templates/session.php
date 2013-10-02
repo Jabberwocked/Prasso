b17aa1ef6ae09c18eb560d65cec8c2d2dff2ae36
teuncortooms <teuncortooms@gmail.com>
2013-10-02 15:08:47 +0200

    2nd try

--- templates/session.php
+++ templates/session.php
@@ -30,7 +30,7 @@ else
 	$_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
 	
 	// This is a new session. Check if logged in.
-	header('Location: ./index.php');
+	header('Location: ./main.php');
     exit();
 }
 ?>
