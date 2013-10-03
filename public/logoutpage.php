1bacb6b2c7d21dfa7609809804192c1dbd2a57b8
teuncortooms <teuncortooms@gmail.com>
2013-10-03 13:53:24 +0200

    idem

--- public/logoutpage.php
+++ public/logoutpage.php
@@ -1,5 +1,3 @@
-<?php header('Location: '.$_SERVER['REQUEST_URI']); ?>
-
 <?php
 include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
@@ -9,7 +7,7 @@ include_once (TEMPLATES_PATH . "/header.php");
 
 session_destroy();
 echo "You are logged out.";
-
+header("Location:main.php");
 ?>
 
 
