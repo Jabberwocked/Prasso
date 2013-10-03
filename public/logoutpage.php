32333a6d02fe3c0ffd001ce2a5ad79c484b7f50c
teuncortooms <teuncortooms@gmail.com>
2013-10-03 13:49:00 +0200

    attempt x

--- public/logoutpage.php
+++ public/logoutpage.php
@@ -1,15 +1,13 @@
-<?php 
-session_destroy();
-header("Location: main.php"); 
-?>
-
 <?php
 include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
 ?>
 
 <?php 
+
+session_destroy();
 echo "You are logged out.";
+header('Location: '.$_SERVER['REQUEST_URI']);
 ?>
 
 
