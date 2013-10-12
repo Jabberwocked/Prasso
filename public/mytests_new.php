90c91e198d225d4868889f4a60c5cdd118b5f2ae
teuncortooms <teuncortooms@gmail.com>
2013-10-13 00:51:34 +0200

    s

--- public/mytests_new.php
+++ public/mytests_new.php
@@ -69,8 +69,14 @@ elseif ($_POST['action'] == "addquestion")
 elseif ($_POST['action'] == "save")
 {
 	echo "saving?<br>";
+
+	try {
 	$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); }
+	catch (PDOException $e) {
+		echo 'Connection failed: ' . $e->getMessage();
+		exit;
+	}
 	
 	foreach ($_SESSION['questions'] as $questionpobject)
 	{
