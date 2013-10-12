f71d83346969fecc9e0c356b9a7abb19d2683c14
teuncortooms <teuncortooms@gmail.com>
2013-10-12 14:27:04 +0200

    x

--- public/takeatest_random.php
+++ public/takeatest_random.php
@@ -41,7 +41,7 @@ include_once (TEMPLATES_PATH . "/header.php");
 		$sql = "SELECT * FROM Questions WHERE Type IN (".$type.") ORDER BY RAND() LIMIT $number";
 		
 		$testquestions = $db->query($sql);
-		print_r($testquestions);
+		print($testquestions);
 		$_SESSION['testquestions'] = $testquestions;
 		
 		/**
