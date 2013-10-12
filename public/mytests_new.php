8a823cc96dda57ce9c5863bbf40fbe08d180d58c
teuncortooms <teuncortooms@gmail.com>
2013-10-12 20:42:26 +0200

    d

--- public/mytests_new.php
+++ public/mytests_new.php
@@ -7,7 +7,42 @@ include_once (MENU_PATH . "/menu_mytests_new.php");
 
 <div style="margin-left: auto; margin-right: auto; width: 500px">
 		
-	<form action=<?php echo htmlspecialchars('mytests_new.php');?> method="get">
+<?php 
+/** 
+ * Save new questions in SESSION
+ */
+
+class question {
+	public $questionno;
+	public $question;
+	public $type;
+	public $answer1;
+	
+	function __construct($questionno, $question, $type, $answer1)
+	{
+		$this->questionno = $questionno;
+		$this->question = $question;
+		$this->type = $type;
+		$this->answer1 = $answer1;
+	}
+	
+	function show()
+	{
+		echo "<p>Question" . $this->questionno . "</p>";
+		echo "<p>Question: " . $this->question . "</p>";
+		echo "<p>Type: " . $this->type . "</p>";
+		echo "<p>Answer1: " . $this->answer1 . "</p>";
+	}
+}
+
+$question1 = new question("a", "b", "c", "d");
+$question1->show();
+
+?>		
+		
+		
+		
+	<form action=<?php echo htmlspecialchars('mytests_new.php');?> method="post">
 		Question<br> 
 		<input type="text" name="question"><br>
 		<input type="radio" style="display:inline; width:20px;" name="type" value="shortanswer" checked>Short Answer<br>
