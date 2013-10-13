ee45e4d8929505d1e39fc8a0f206c3553de97e32
teuncortooms <teuncortooms@gmail.com>
2013-10-13 19:26:33 +0200

    c

--- templates/menu/menu_main.php
+++ templates/menu/menu_main.php
@@ -1,13 +1,28 @@
+<?php if (!isset($_SESSION['username'])) { ?>
 
+			<div style="text-align:center">
+				<nav>
+					<a href="takeatest.php">Take a test</a><br>
+					<a href="results.php">Results</a><br>
+					<br>
+					<br>
+					<a href="mytests.php">My Tests</a><br>			
+		<!-- 		<a href="other.php">Other</a><br>		-->
+				</nav>
+			</div>
 
-	<div style="text-align:center">
-		<nav>
-			<a href="takeatest.php">Take a test</a><br>
-			<a href="results.php">Results</a><br>
-			<br>
-			<br>
-			<a href="mytests.php">My Tests</a><br>			
-<!-- 		<a href="other.php">Other</a><br>		-->
-		</nav>
-	</div>
+<?php } else { ?>
 
+			<div style="text-align:center">
+				<nav>
+					<a href="takeatest.php">Take a test</a><br>
+					<a href="results.php">Results</a><br>
+					<br>
+					<br>
+					<a href="mytests.php">My Tests</a><br>			
+		<!-- 		<a href="other.php">Other</a><br>		-->
+				</nav>
+			</div>
+		
+<?php } ?>	
+	
