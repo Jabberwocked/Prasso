d05bf3ad5ffd3315c6db2dfb6dd7a664d87f38b9
teuncortooms <teuncortooms@gmail.com>
2013-10-13 19:21:06 +0200

    d

--- public/main.php
+++ public/main.php
@@ -1,6 +1,6 @@
 <?php
 include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
-include_once (MENU_PATH . "/menu_main_loggedin.php");
+include_once (MENU_PATH . "/menu_main.php");
 include_once (TEMPLATES_PATH . "/footer.php");
 ?>
