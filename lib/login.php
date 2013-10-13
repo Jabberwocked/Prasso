802513315754af3fc675241776b7d8ab14b3eaf5
teuncortooms <teuncortooms@gmail.com>
2013-10-13 20:32:51 +0200

    c

--- lib/login.php
+++ lib/login.php
@@ -6,11 +6,19 @@
 	<div id="main-wrapper">
 		<div id="login-wrapper">
 			<form method="post" action="">
-				<label for="usn">Username : </label> 
-				<input type="text" maxlength="30" required autofocus name="username" />
-				<label for="passwd">Password : </label> <input type="password" maxlength="30" required name="password" />
-				<button type="submit" name="login" value="Log me in">Log me in</button> 
-				<button type="button" name="register" value="Register" onclick="location.href='registerpage.php'">Register</button>
+				<ul>
+					<li>
+						<label for="usn">Username : </label> 
+						<input type="text" maxlength="30" required autofocus name="username" />
+					</li>
+					<li>
+						<label for="passwd">Password : </label> <input type="password" maxlength="30" required name="password" />
+					</li>
+					<li class="buttons">
+						<input type="submit" name="login" value="Log me in" /> 
+						<input type="button" name="register" value="Register" onclick="location.href='registerpage.php'" />
+					</li>
+				</ul>
 			</form>
 
 		</div>
