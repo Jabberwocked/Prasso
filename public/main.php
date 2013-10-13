fa64554fa9e36ca4e9995a06515149b3a82f6e8f
teuncortooms <teuncortooms@gmail.com>
2013-10-13 15:57:42 +0200

    d

--- public/main.php
+++ public/main.php
@@ -3,8 +3,9 @@ include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
 include_once (MENU_PATH . "/menu_main_loggedin.php");
 
-echo "test";
+echo "test<br>";
 print_r($_SESSION['test']);
+echo "<br>" . $_SESSION['test'];
 
 include_once (TEMPLATES_PATH . "/footer.php");
 ?>
