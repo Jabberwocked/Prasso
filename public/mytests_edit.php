f610a7c2d97d4ee6ba29f8f0701bb64ad60b514c
teuncortooms <teuncortooms@gmail.com>
2013-10-13 14:17:07 +0200

    a

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -13,34 +13,34 @@ include_once (TEMPLATES_PATH . "/header.php");
 
 
 
-// /**
-//  * Class question
-//  */
+/**
+ * Class question
+ */
 
 
-// class question {
-// 	public $questionno;
-// 	public $question;
-// 	public $type;
-// 	public $answer1;
+class question {
+	public $questionno;
+	public $question;
+	public $type;
+	public $answer1;
 	
-// 	function __construct($questionno, $question, $type, $answer1)
-// 	{
-// 		$this->questionno = $questionno;
-// 		$this->question = $question;
-// 		$this->type = $type;
-// 		$this->answer1 = $answer1;
-// 	}
+	function __construct($questionno, $question, $type, $answer1)
+	{
+		$this->questionno = $questionno;
+		$this->question = $question;
+		$this->type = $type;
+		$this->answer1 = $answer1;
+	}
 	
-// 	function show()
-// 	{
-// 		echo "<p style='font-weight:bold'>Question " . $this->questionno . "</p>";
-// 		echo "<p>Question: " . $this->question . "</p>";
-// 		echo "<p>Type: " . $this->type . "</p>";
-// 		echo "<p>Answer1: " . $this->answer1 . "</p>";
-// 		echo "<br><br>";
-// 	}
-// }
+	function show()
+	{
+		echo "<p style='font-weight:bold'>Question " . $this->questionno . "</p>";
+		echo "<p>Question: " . $this->question . "</p>";
+		echo "<p>Type: " . $this->type . "</p>";
+		echo "<p>Answer1: " . $this->answer1 . "</p>";
+		echo "<br><br>";
+	}
+}
 
 
 
