6ebef38ea0c560e8cb2b195bf249db9ef49a3606
teuncortooms <teuncortooms@gmail.com>
2013-10-13 20:11:14 +0200

    c

--- templates/menu/menu_takeatest.php
+++ templates/menu/menu_takeatest.php
@@ -1,9 +1,28 @@
 
+<?php if (!isset($_SESSION['username'])) { ?>
+
 	<div style="text-align:center">
 		<nav>
 			<a href="takeatest_random.php">Random by topic</a><br>
-			<a href="mytests.php">My tests <!-- and shared tests --> </a><br>
-			<a href="">Administered tests</a>
+			<a href="">Administered tests</a><br>
+			<br>
+			<a href="loginpage.php" style="color:grey">My tests <!-- and shared tests --> </a><br>
 		</nav>
 	</div>
 
+
+<?php } else { ?>
+
+
+	<div style="text-align:center">
+		<nav>
+			<a href="takeatest_random.php">Random by topic</a><br>
+			<a href="">Administered tests</a><br>
+			<br>
+			<a href="mytests.php">My tests <!-- and shared tests --> </a><br>
+		</nav>
+	</div>
+
+		
+<?php } ?>	
+	
