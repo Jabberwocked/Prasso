96d4bbea3785d9207dc20f286c21a80219fad577
teuncortooms <teuncortooms@gmail.com>
2013-10-13 18:22:21 +0200

    dd

--- templates/menu/menu_takeatest.php
+++ templates/menu/menu_takeatest.php
@@ -2,7 +2,7 @@
 	<div style="text-align:center">
 		<nav>
 			<a href="takeatest_random.php">Random by topic</a><br>
-			<a href="">My tests <!-- and shared tests --> </a><br>
+			<a href="mytests.php">My tests <!-- and shared tests --> </a><br>
 			<a href="">Administered tests</a>
 		</nav>
 	</div>
