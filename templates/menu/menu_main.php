eab34c710059bd97ad329d888cb5437c1f9ec8fe
teuncortooms <teuncortooms@gmail.com>
2013-10-13 20:50:45 +0200

    c

--- templates/menu/menu_main.php
+++ templates/menu/menu_main.php
@@ -4,6 +4,7 @@
 				<nav>
 					<a href="takeatest.php">Take a test</a><br>
 					<br>
+					<br>
 					<a href="loginpage.php">Log in</a><br>
 				</nav>
 			</div>
@@ -13,9 +14,9 @@
 			<div style="text-align:center">
 				<nav>
 					<a href="takeatest.php">Take a test</a><br>
+					<br>
+					<br>
 					<a href="results.php">Results</a><br>
-					<br>
-					<br>
 					<a href="mytests.php">My Tests</a><br>			
 		<!-- 		<a href="other.php">Other</a><br>		-->
 				</nav>
