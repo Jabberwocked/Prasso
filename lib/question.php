40da35cd7a5396ea589c585b0b4fe1cbc926dc69
teuncortooms <teuncortooms@gmail.com>
2013-10-13 14:01:23 +0200

    moved class question to lib

---
+++ lib/question.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Class question
+ */
+
+
+class question {
+	public $questionno;
+	public $question;
+	public $type;
+	public $answer1;
+
+	function __construct($questionno, $question, $type, $answer1)
+	{
+		$this->questionno = $questionno;
+		$this->question = $question;
+		$this->type = $type;
+		$this->answer1 = $answer1;
+	}
+
+	function show()
+	{
+		echo "<p style='font-weight:bold'>Question " . $this->questionno . "</p>";
+		echo "<p>Question: " . $this->question . "</p>";
+		echo "<p>Type: " . $this->type . "</p>";
+		echo "<p>Answer1: " . $this->answer1 . "</p>";
+		echo "<br><br>";
+	}
+}
+
+
+?>
