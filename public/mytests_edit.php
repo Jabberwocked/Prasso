e6d60c1607b0268c7f33d40a21c026716a8047bb
teuncortooms <teuncortooms@gmail.com>
2013-10-15 20:54:34 +0200

    d

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -152,8 +152,8 @@ else
 		<form action=<?php echo htmlspecialchars('mytests_edit.php');?> method="post">
 		Test Name<br>
 		<input type="text" name="testname" placeholder="Give your test a name." style="display:inline; width:50%">
-		<button type="submit" name="action" value="save" style="width:6em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Save</button>
-		<button type="submit" name="action" value="deleteall" style="width:6em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Delete</button>
+		<button type="submit" name="action" value="save" style="width:4em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Save</button>
+		<button type="submit" name="action" value="deleteall" style="width:4em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Delete</button>
 		</form>
 		<br>
 		<br>
