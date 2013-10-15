04d9a8da87bb9046e0600a90e4883abb57cb3b25
teuncortooms <teuncortooms@gmail.com>
2013-10-15 18:04:41 +0200

    idem index.php

--- public/index.php
+++ public/index.php
@@ -11,26 +11,19 @@ if (isset($_SESSION['HTTP_USER_AGENT']))
 	{
 		// Someone is attempting hijacking of the user agent.
 		session_destroy();
-    	header('Location: ./loginpage.php');
+    	header('Location: ./main.php');
     	exit();   
 	}
 	else
 	{
-		if( !(isset( $_POST['login'] ) ) )
-		{
-		header('Location: ./loginpage.php');
-		}
-		else
-		{
-			echo ("logged in");
-		}
+		// User is already logged in. Continue session.
 	}
 }
 else
+// New session.
 {
 	$_SESSION['HTTP_USER_AGENT'] = md5($_SERVER['HTTP_USER_AGENT']);
-	
-	// This is a new session. Check if logged in.
+
 	header('Location: ./main.php');
     exit();
 }
