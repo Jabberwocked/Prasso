82169b6ce4637d0727845e1c88eac03b1c5b941d
teuncortooms <teuncortooms@gmail.com>
2013-10-15 20:23:27 +0200

    d

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -146,7 +146,7 @@ else
 			<input type="text" name="question" placeholder="Question text">
 			<input type="text" name="answers[]" class="answers" placeholder="Answer 1" style="display:inline; width:50%">
 			<button type="button" id="addOption" value="Add" style="width:2em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0; ">+</button> 
-			<button type="submit" name="action" value="addquestion" style="width:5em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Add Question</button><br>
+			<button type="submit" name="action" value="addquestion" style="width:5em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Add</button><br>
 		</form>
 		<br>
 		<br>
