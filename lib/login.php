2e9faaf86e516c2c2c74958cdc86ff1f05561587
teuncortooms <teuncortooms@gmail.com>
2013-10-15 18:41:08 +0200

    d

--- lib/login.php
+++ lib/login.php
@@ -12,7 +12,7 @@
 				}
 				else
 				{ 
-					echo "<p style='color:green'> Temporary line. </p>";
+					echo "<p style='color:green; text-align:center'> Temporary line. </p>";
 				}
 		?>
 		<div id="login-wrapper">
