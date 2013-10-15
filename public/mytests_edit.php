3283450823dc542ed60fbd7ff679b7d7cef101b8
teuncortooms <teuncortooms@gmail.com>
2013-10-15 20:57:49 +0200

    d

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -144,8 +144,8 @@ else
 				<option value="multichoice">MC: Multiple Choice</option>
 			</select> 
 			<input type="text" name="answers[]" class="answers" placeholder="Answer 1" style="display:inline; width:60%">
-			<button type="button" id="addOption" value="Add" style="width:2em; height:2.5em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0; ">+</button> 
-			<button type="submit" name="action" value="addquestion" style="width:5em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Add</button><br>
+			<button type="button" id="addOption" value="Add" style="width:2em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0; ">+</button> 
+			<button type="submit" name="action" value="addquestion" style="width:5em; height:2.5em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;">Add</button><br>
 		</form>
 		<br>
 		<br>
