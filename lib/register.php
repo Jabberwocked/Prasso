401cee787a0dfb39d112a18c6d72046b8e274f2d
teuncortooms <teuncortooms@gmail.com>
2013-10-17 00:11:12 +0200

    d

--- lib/register.php
+++ lib/register.php
@@ -7,10 +7,13 @@
 			<form method="post" action="">
 				<label for="usn">Username : </label> 
 				<input type="text" id="usn" maxlength="30" required autofocus name="username" />
+				<br>
 				<label for="passwd">Password : </label> 
 				<input type="password" id="passwd" maxlength="30" required name="password" />
+				<br>
 				<label for="conpasswd">Confirm Password : </label> 
 				<input type="password" id="conpasswd" maxlength="30" required name="conpassword" />
+				<br>
 				<button type="submit" name="register" value="Register">Register</button> |
 				<button type="button" name="cancel" value="Cancel" onclick="location.href='index.php'">Cancel</button>
 			</form>
