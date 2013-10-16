c9158b5676952688e0c18b06e564e3ef70b7b910
teuncortooms <teuncortooms@gmail.com>
2013-10-16 16:52:03 +0200

    d

--- lib/question.php
+++ lib/question.php
@@ -34,7 +34,7 @@ class question {
 	{
 		echo "<p style='font-weight:bold'>" . $this->questionno . " " . $this->question ;
 		echo " <span style='font-weight:normal'> (" . $this->typeshort ;
-		echo ") - <form><button style='display:inline; height:0.5em; width:2em'>edit</button></form></span></p>";
+		echo ") - <form><button style='width:2em; height:2em; margin:0 0 0 0; padding:0 0 0 0; border: 0 0 0 0;'>edit</button></form></span></p>";
 		
 		echo "<p>Answers: " ;
 		$n = 1;
