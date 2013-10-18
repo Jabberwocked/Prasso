65b9ee105fbdb45faeeb561e041f6072b25bf171
teuncortooms <teuncortooms@gmail.com>
2013-10-18 18:45:08 +0200

    d

--- lib/test.php
+++ lib/test.php
@@ -11,65 +11,65 @@ class test
 		echo "lalalala";
 	}
 	
-// 	function edit($testid)
-// 	{
-// 		/** 
-// 		 * Pull test name to session
-// 		 */
+	function edit($testid)
+	{
+		/** 
+		 * Pull test name to session
+		 */
 		
-// 		$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
-// 		$sql = "SELECT * FROM Tests WHERE TestId=".$testid;
-// 		$result = $db->query($sql);
-// 		foreach ($result as $test)
-// 		{
-// 			$_SESSION['testname'] = $test['TestName'];
-// 		}
+		$db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+		$sql = "SELECT * FROM Tests WHERE TestId=".$testid;
+		$result = $db->query($sql);
+		foreach ($result as $test)
+		{
+			$_SESSION['testname'] = $test['TestName'];
+		}
 		
-// 		/**
-// 		 * Pull questionids to session
-// 		 */
+		/**
+		 * Pull questionids to session
+		 */
 		
-// 		$_SESSION['questionobjects'] = array();
+		$_SESSION['questionobjects'] = array();
 		
-// 		$sql = "SELECT * FROM Question_Test WHERE TestId=".$testid." ORDER BY OrderNo";
-// 		$result = $db->query($sql);
+		$sql = "SELECT * FROM Question_Test WHERE TestId=".$testid." ORDER BY OrderNo";
+		$result = $db->query($sql);
 		
-// 		foreach ($result as $relation)
-// 		{
-// 			$questionids[] = $relation['QuestionId'];
-// 		}
+		foreach ($result as $relation)
+		{
+			$questionids[] = $relation['QuestionId'];
+		}
 		
-// 		/**
-// 		 * Pull questions and answers to session and save as questionobjects
-// 		 */
+		/**
+		 * Pull questions and answers to session and save as questionobjects
+		 */
 		
-// 		$questionno = 1;
-// 		foreach ($questionids as $questionid)
-// 		{
-// 			$sql = "SELECT * FROM Questions WHERE QuestionId=".$questionid;
-// 			$resultquestions = $db->query($sql);
+		$questionno = 1;
+		foreach ($questionids as $questionid)
+		{
+			$sql = "SELECT * FROM Questions WHERE QuestionId=".$questionid;
+			$resultquestions = $db->query($sql);
 		
-// 			$sql = "SELECT * FROM Answers WHERE QuestionId=".$questionid;
-// 			$resultanswers = $db->query($sql);
+			$sql = "SELECT * FROM Answers WHERE QuestionId=".$questionid;
+			$resultanswers = $db->query($sql);
 		
-// 			$answerarray = array();
-// 			foreach ($resultanswers as $tempanswersobject)
-// 			{
-// 				$tempanswersarray[] = $tempanswersobject['Answer'];
-// 			}
+			$answerarray = array();
+			foreach ($resultanswers as $tempanswersobject)
+			{
+				$tempanswersarray[] = $tempanswersobject['Answer'];
+			}
 		
 		
-// 			foreach ($resultquestions as $tempquestionobject)
-// 			{
-// 				$_SESSION['questionobjects'][] = new questionobject(array(	'questionno' => $questionno,
-// 					'question' => $tempquestionobject['Question'],
-// 					'type' => $tempquestionobject['Type'],
-// 					'answers' => $tempanswersarray));
-// 			}
-// 			$questionno ++;
+			foreach ($resultquestions as $tempquestionobject)
+			{
+				$_SESSION['questionobjects'][] = new questionobject(array(	'questionno' => $questionno,
+					'question' => $tempquestionobject['Question'],
+					'type' => $tempquestionobject['Type'],
+					'answers' => $tempanswersarray));
+			}
+			$questionno ++;
 		
-// 		}	
-// 	}
+		}	
+	}
 	
 	
 	
