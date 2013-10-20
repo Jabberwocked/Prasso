8fee3a463e132857303c055e58ec3c232a8349c1
teuncortooms <teuncortooms@gmail.com>
2013-10-20 16:24:44 +0200

    d

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -26,7 +26,7 @@ if (isset($_POST['testtoedit']))
  * If an item to edit was selected, save it to session.
  */
 
-if (!isset($_POST['itemtoedit']))
+if (isset($_POST['itemtoedit']))
 {
 	$_SESSION['itemtoedit'] = $_POST['itemtoedit'];
 }
