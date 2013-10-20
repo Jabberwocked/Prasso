a655a7ab96e9d502e6f5a313a349d6a6eabbe693
teuncortooms <teuncortooms@gmail.com>
2013-10-20 11:27:33 +0200

    d

--- public/test123.php
+++ public/test123.php
@@ -7,13 +7,13 @@ include_once (TEMPLATES_PATH . "/header.php");
 ?>
 
 <script>
-$(document).ready(function(){
+// $(document).ready(function(){
 
-// Save question (is submit form) when another question is clicked. Doesn't work.
-	$("button").click(function() {
-	$('#question4').submit();
+// // Save question (is submit form) when another question is clicked. Doesn't work.
+// 	$("button").click(function() {
+// 	$('#question4').submit();
 
-});
+// });
 </script> 	
 
 
@@ -21,6 +21,7 @@ $(document).ready(function(){
 	<button type=button name=1 value=on>Example question 1</button><br>
 	<button type=button name=2 value=on>Example question 2</button><br>
 	<button type=submit name=3 value=on>Example question 3</button><br>
-<form id='question4' method="get">
+<form id='question4' method="get" action="test123.php">
 	<input type=text name=4 placeholder='Example question 4'>
+	<button type='submit'>Submit</button>
 </form>
