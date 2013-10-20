6cb6a49baa863eb80c5c325bc3b0d3e29fb7eed3
teuncortooms <teuncortooms@gmail.com>
2013-10-20 10:33:33 +0200

    d

--- public/test.php
+++ public/test.php
@@ -8,8 +8,8 @@ include_once (TEMPLATES_PATH . "/footer.php");
 
 
 <form id='questionform' action="test.php" method="get">
-	<button name=1 value=on>asdfasdf</button>
-	<button name=2 value=on>asdfas</button>
-	<button name=3 value=on>asdfsa</button>
+	<button type=button name=1 value=on>asdfasdf</button>
+	<button type=button name=2 value=on>asdfas</button>
+	<button type=button name=3 value=on>asdfsa</button>
 	<input type=text name=lala>
 </form>
