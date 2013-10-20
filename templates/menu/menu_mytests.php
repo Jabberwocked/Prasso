a5f210e61a130d18241b629bdcb471600708deec
teuncortooms <teuncortooms@gmail.com>
2013-10-20 08:06:20 +0200

    d

--- templates/menu/menu_mytests.php
+++ templates/menu/menu_mytests.php
@@ -1,8 +1,8 @@
 	<div style="text-align:center">
-		<nav style="margin: 10px 20px 40px 20px;">
-			<a href="mytests_edit.php">Selftest</a>
-			<a href="mytests_edit.php">Code</a>
-			<a href="mytests_edit.php">My Tests</a>
+		<nav>
+			<a href="takeatest_random.php">Selftest</a>
+			<a href="">Code</a>
+			<a href="mytests.php">My Tests</a>
 		</nav>
 		
 	</div>
