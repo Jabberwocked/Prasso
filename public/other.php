595cc9c01822786e5f0264f32a918740368f1ce5
teuncortooms <teuncortooms@gmail.com>
2013-10-20 09:46:38 +0200

    d

--- public/other.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once ("../config/config.php");
-include_once (TEMPLATES_PATH . "/header.php");
-
-include_once (MENU_PATH . "/menu_other.php");
-
-include_once (TEMPLATES_PATH . "/footer.php");
-?>
--- templates/menu/menu_other.php
+++
@@ -1,6 +0,0 @@
-
-		<nav>
-			<a href="">Create a timeline</a><br>
-			<a href="">Search lesson blocks by topic</a><br>
-		</nav>
-
