335f9ea453d3fc2e29488fa824f1104aeccdb824
teuncortooms <teuncortooms@gmail.com>
2013-10-20 09:10:57 +0200

    c

--- templates/menu/menu_mytests.php
+++ templates/menu/menu_mytests.php
@@ -1,4 +1,11 @@
-
+<style>
+	nav a[href=<?php echo $_SERVER['PHP_SELF']?>] {
+	color: orange;
+	outline: none;
+	border-bottom: 2px solid #eee; /* Bottom border style on hover */
+}
+</style>
+<?php echo $_SERVER['PHP_SELF']?>
 		<nav>
 			<a href="takeatest_random.php">Selftest</a>
 			<a href="">Code</a>
