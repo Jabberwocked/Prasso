ee3eac85545e8e69ddb2262fa07074beb4887456
teuncortooms <teuncortooms@gmail.com>
2013-10-20 09:32:43 +0200

    c

--- templates/menu/menu_mytests.php
+++ templates/menu/menu_mytests.php
@@ -10,7 +10,6 @@
 
 		<nav>
 			<a href="takeatest_random.php">Selftest</a>
-			<a href="">Code</a>
 			<?php if (!isset($_SESSION['username'])) { ?>
 			<a href="mytests.php" style="color:lightgrey">My Tests</a>
 			<a href="mytests_edit.php" style="color:lightgrey">New</a><br>
@@ -18,5 +17,11 @@
 			<a href="mytests.php">My Tests</a>
 			<a href="mytests_edit.php">New</a><br>
 			<?php } ?>
+			
+<!-- 		TEMP	 -->
+			<?php if ($invitation) {?>
+			<a href="">Invitations</a>
+			<?php };?>
+			
 		</nav>
 	
