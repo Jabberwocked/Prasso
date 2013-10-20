9c6b303186726e15e966276b6af5095d0b96d8de
teuncortooms <teuncortooms@gmail.com>
2013-10-20 08:16:22 +0200

    X

--- templates/menu/menu_mytests.php
+++ templates/menu/menu_mytests.php
@@ -2,14 +2,12 @@
 		<nav>
 			<a href="takeatest_random.php">Selftest</a>
 			<a href="">Code</a>
-			<a href="mytests.php">My Tests</a>
-			<a href="mytests_edit.php">New</a><br>
 			<?php if (!isset($_SESSION['username'])) { ?>
-			<a href="mytests.php">My Tests</a>
-			<a href="mytests_edit.php">New</a><br>
-			<?php } else { ?>
 			<a href="mytests.php" style="color:lightgrey">My Tests</a>
 			<a href="mytests_edit.php" style="color:lightgrey">New</a><br>
+			<?php } else { ?>
+			<a href="mytests.php">My Tests</a>
+			<a href="mytests_edit.php">New</a><br>
 			<?php } ?>
 		</nav>
 		
