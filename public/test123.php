0a6b9560edf68564c7d14230a5a196c1d6dbd9dd
teuncortooms <teuncortooms@gmail.com>
2013-10-20 11:20:45 +0200

    d

--- public/test123.php
+++ public/test123.php
@@ -17,9 +17,9 @@ $(document).ready(function(){
 </script> 
 
 <form action="test.php" method="get">
-	<button type=button name=1 value=on>Example question 1</button>
-	<button type=button name=2 value=on>Example question 2</button>
-	<button type=submit name=3 value=on>Example question 3</button>
+	<button type=button name=1 value=on>Example question 1</button><br>
+	<button type=button name=2 value=on>Example question 2</button><br>
+	<button type=submit name=3 value=on>Example question 3</button><br>
 <form id='question4' method="get">
 	<input type=text name=4 placeholder='Example question 4'>
 </form>
