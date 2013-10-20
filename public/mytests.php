199c441ca364eb4f50175e0eb8ff5884b9d75a68
teuncortooms <teuncortooms@gmail.com>
2013-10-20 08:04:06 +0200

    d

--- public/mytests.php
+++ public/mytests.php
@@ -4,6 +4,21 @@ include_once (TEMPLATES_PATH . "/header.php");
 include_once (MENU_PATH . "/menu_mytests.php");
 ?>
 
+	<div style="text-align:center">
+		<nav style="margin: 10px 20px 40px 20px;">
+			<a href="mytests_edit.php">New</a><br>
+		</nav>
+		
+		<nav>
+			<a href="" style="font-size: small">Show all tests</a>
+			<a href="" style="font-size: small">Show in folders</a>
+			<div style="display:inline; margin: 0px 0px 0px 20px">Filter by:</div>
+			<a href="" style="font-size: small; margin: 10px 2px">topic</a>
+			<a href="" style="font-size: small; margin: 10px 2px">label</a>
+		</nav>
+		
+	</div>
+
 <style type="text/css">
 td {
 	padding: 0px 10px
--- templates/menu/menu_mytests.php
+++ templates/menu/menu_mytests.php
@@ -1,14 +1,8 @@
 	<div style="text-align:center">
 		<nav style="margin: 10px 20px 40px 20px;">
-			<a href="mytests_edit.php">New</a><br>
-		</nav>
-		
-		<nav>
-			<a href="" style="font-size: small">Show all tests</a>
-			<a href="" style="font-size: small">Show in folders</a>
-			<div style="display:inline; margin: 0px 0px 0px 20px">Filter by:</div>
-			<a href="" style="font-size: small; margin: 10px 2px">topic</a>
-			<a href="" style="font-size: small; margin: 10px 2px">label</a>
+			<a href="mytests_edit.php">Selftest</a>
+			<a href="mytests_edit.php">Code</a>
+			<a href="mytests_edit.php">My Tests</a>
 		</nav>
 		
 	</div>
