3767400775cd2dfc65bf8659d30d62a19da07044
teuncortooms <teuncortooms@gmail.com>
2013-10-20 10:29:50 +0200

    d

---
+++ public/test.php
@@ -0,0 +1,14 @@
+<?php
+include_once ("../config/config.php");
+include_once (TEMPLATES_PATH . "/header.php");
+
+include_once (TEMPLATES_PATH . "/footer.php");
+?>
+
+
+
+<form id='questionform' action="test.php" method="get">
+	<button name=1 value=on>asdfasdf</button>
+	<button name=2 value=on>asdfas</button>
+	<button name=3 value=on>asdfsa</button>
+</form>
