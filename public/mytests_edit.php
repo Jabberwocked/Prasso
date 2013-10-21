548942aea711f218dd02fbb921ce4725a8430caf
teuncortooms <teuncortooms@gmail.com>
2013-10-21 11:35:55 +0200

    d

--- public/mytests_edit.php
+++ public/mytests_edit.php
@@ -92,7 +92,7 @@ if ($_POST['action2'] == "savetest")
  */
 ?>
 
-<div style="width: 800px; margin: 0px auto; border: 1px dotted; padding: 20px 150px 100px 150px">
+<div style="width: 500px; margin: 0px auto; border: 1px dotted; padding: 20px 170px 100px 170px">
 <div style="margin-left: auto; margin-right: auto; width: 500px">
 <?php $_SESSION['test']->show(); ?>
 </div>
