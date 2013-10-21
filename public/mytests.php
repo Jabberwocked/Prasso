ae657e1a3421382baa59444884bb829af1e317b8
teuncortooms <teuncortooms@gmail.com>
2013-10-21 10:42:13 +0200

    d

--- public/mytests.php
+++ public/mytests.php
@@ -18,8 +18,8 @@ td {
 			<a href="" style="font-size: small">Show all tests</a>
 			<a href="" style="font-size: small">Show in folders</a>
 			<div style="display:inline; margin: 0px 0px 0px 20px">Filter by:</div>
-			<a href="" style="font-size: small; margin: 10px 2px">topic</a>
-			<a href="" style="font-size: small; margin: 10px 2px">label</a>
+			<a href="" style="font-size: small; margin: 0px 2px">topic</a>
+			<a href="" style="font-size: small; margin: 0px 2px">label</a>
 		</nav>
 	</div>
 	<table>
