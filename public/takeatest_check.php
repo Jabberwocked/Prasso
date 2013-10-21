cd4dc0dae6035f37813f25ead093f40f795d91e6
teuncortooms <teuncortooms@gmail.com>
2013-10-21 10:18:07 +0200

    d

--- public/takeatest_check.php
+++ public/takeatest_check.php
@@ -89,8 +89,8 @@ foreach($questionids as $orderno => $id)
 		$colour = "lightcoral";
 	}
 	
-	echo "<div style='display: block; float:right'> Score: " . $score . "</span> | <span> Answer: "; $n = 1; foreach ($answers[$id] as $answer){if ($n > 1){echo ", ";}$n ++;echo $answer;};echo "</div>";
-	echo "<p style='background-color:" . $colour . "'><span style='font-weight:bold;'> ".$orderno.". ".$questions[$id]."</span><br>";
+	echo "<p style='background-color:" . $colour . "'><div style='display: block; float:right'> Score: " . $score . "</span> | <span> Answer: "; $n = 1; foreach ($answers[$id] as $answer){if ($n > 1){echo ", ";}$n ++;echo $answer;};echo "</div>";
+	echo "<span style='font-weight:bold;'> ".$orderno.". ".$questions[$id]."</span><br>";
 	echo "<span>" . $useranswers[$id] . "</span></p><br>";
 	
 
