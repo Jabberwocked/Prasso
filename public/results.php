3cd8ab63695c935d9667b68c4fa8f2a38b9b5a0a
teuncortooms <teuncortooms@gmail.com>
2013-10-23 11:28:51 +0200

    d

--- public/results.php
+++ public/results.php
@@ -15,8 +15,19 @@ else {
 
 
 
-
+	<nav>
+		<a href="" style="font-size: small">Show all results</a>
+		<a href="" style="font-size: small">Show in folders</a>
+		<!-- <a href="" style="font-size: small">Show paths</a> -->
+		<div style="display:inline; margin: 0px 0px 0px 20px">Filter by:</div>
+		<a href="" style="font-size: small; margin: 10px 2px">topic</a>
+		<a href="" style="font-size: small; margin: 10px 2px">label</a>
+		<a href="" style="font-size: small; margin: 10px 2px">user</a>
+		<a href="" style="font-size: small; margin: 10px 2px">test</a>
+	</nav>
+	
 <!-- PLACEHOLDER -->
+	
 	
 <style type="text/css">
 	td {
--- templates/menu/menu_results.php
+++ templates/menu/menu_results.php
@@ -1,14 +1,5 @@
 
 
-	<nav>
-		<a href="" style="font-size: small">Show all results</a>
-		<a href="" style="font-size: small">Show in folders</a>
-		<!-- <a href="" style="font-size: small">Show paths</a> -->
-		<div style="display:inline; margin: 0px 0px 0px 20px">Filter by:</div>
-		<a href="" style="font-size: small; margin: 10px 2px">topic</a>
-		<a href="" style="font-size: small; margin: 10px 2px">label</a>
-		<a href="" style="font-size: small; margin: 10px 2px">user</a>
-		<a href="" style="font-size: small; margin: 10px 2px">test</a>
-	</nav>
+
 
 
