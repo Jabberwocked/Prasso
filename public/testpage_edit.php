e4e416f2b09bdbfeef66be4f07cffcaf7c80ac48
teuncortooms <teuncortooms@gmail.com>
2014-04-02 22:38:22 +0200

    d

--- public/testpage_edit.php
+++ public/testpage_edit.php
@@ -74,6 +74,7 @@ elseif ($_POST['action'] == "save")
 
 if ($_POST['action2'] == "savetest")
 {
+	echo "lulu";
 	if ($this->testname == false)
 	{
 		echo "<p style='color:red'>Please insert a test name</p><br>";
