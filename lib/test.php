60148d17cf234604124eae36021330953d7309c4
teuncortooms <teuncortooms@gmail.com>
2014-04-02 20:40:52 +0200

    d

--- lib/test.php
+++ lib/test.php
@@ -323,7 +323,7 @@ class test
 			// remove empty answer strings
 			$emptyanswers = array_keys($this->questionobjects[$_POST['orderno']]->answers, "");
 			print_r($emptyanswers);
-			echo " lala" ;
+			echo " lala2" ;
 			foreach ($emptyanswers as $key)
 			{
 				if ($key != '')
