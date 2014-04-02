9f6d1d71ef0ab40d812e0806afc3e7e79494310b
Teun <teuncortooms@gmail.com>
2014-04-02 18:01:18 +0200

    d

--- lib/test.php
+++ lib/test.php
@@ -331,7 +331,7 @@ class test
 			$this->questionobjects[$_POST['orderno']] = new questionobject($_POST);
 			// remove empty answer strings
 			$emptyanswers[] = array_search('', $this->questionobjects[$_POST['orderno']]->answers);
-			print_r $emptyanswers;
+			print_r($emptyanswers);
 			foreach ($emptyanswers as $key)
 			{
 				unset($this->questionobjects[$_POST['orderno']]->answers[$key]);
