f71b8ad1272156eb1cf37fe38b1f0438b2a083c4
teuncortooms <teuncortooms@gmail.com>
2014-04-02 20:19:31 +0200

    d

--- lib/test.php
+++ lib/test.php
@@ -286,12 +286,13 @@ class test
 			
 			<br>
 		<div style="position: absolute; bottom: 50px">
-			<button type="submit" name="action2" value="savetest">Save Test</button>
-			|
-			<button type="submit" name="action" value="reset">Reset</button>
 			<?php if ($_POST['action2'] == "savetest") { ?>
 			<p style='font-weight:bold; color:green'>Test is saved.2</p>
 			<?php } ?>
+			<button type="submit" name="action2" value="savetest">Save Test</button>
+			|
+			<button type="submit" name="action" value="reset">Reset</button>
+
 		</div>
 	
 		
