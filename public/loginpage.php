24f84c5af1860d3019121d592a3ac771019c817c
Jabberwocked <de_jabberwock@hotmail.com>
2014-04-18 09:19:03 +0200

    just testing

--- public/loginpage.php
+++ public/loginpage.php
@@ -1,7 +1,7 @@
 <?php
 include_once ("../config/config.php");
 include_once (TEMPLATES_PATH . "/header.php");
-include_once (LIBRARY_PATH . "/login.php");
+//include_once (LIBRARY_PATH . "/login.php");
 include_once (TEMPLATES_PATH . "/footer.php");
 ?>
 
